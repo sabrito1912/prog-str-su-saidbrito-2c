7355191fa224f699fef5518af356962b850720c1
sabrito1912 <20253ds090@utez.edu.mx>
2026-01-14 23:09:18 -0600

    feat: agregar operaciones

--- MergeWithOutConflict/src/MAin.java
+++ MergeWithOutConflict/src/MAin.java
@@ -2,5 +2,7 @@ public class MAin {
     public static void main(String[] args) {
         System.out.println("Semana 2");
         System.out.println("Versión base");
+        System.out.println("2 + 2 = 4");
+        System.out.println("5 * 3 = 15");
     }
 }
