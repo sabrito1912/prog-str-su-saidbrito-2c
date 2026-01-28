884a361c2a0a91a115321c7df008083a537d975a
sabrito1912 <20253ds090@utez.edu.mx>
2026-01-28 09:30:14 -0600

    Update de TarifaIfElse

--- ControlIfElseSaidBrito/src/ActividadIfElseTarifa.java
+++ ControlIfElseSaidBrito/src/ActividadIfElseTarifa.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ControlIfElseSaidBrito {
+public class ActividadIfElseTarifa {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Ingresa tu edad: ");
